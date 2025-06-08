f044546bfebf5dd2c6961cb80f2f55e58ca2ccda
Steven Kim <stevenn.kkim@gmail.com>
2025-06-08 13:38:19 -0400

    update description

--- src/data/AdmissionInfo.tsx
+++ src/data/AdmissionInfo.tsx
@@ -16,8 +16,8 @@ export const AdmissionInfo: AdmissionInfo[] = [
 		title: '招生对象 | Age Group',
 		body: (
 			<>
-				<p>2-6岁</p>
-				<p>2-6 years old</p>
+				<p>0-4岁</p>
+				<p>0-4 years old</p>
 			</>
 		),
 		icon: Cake,
@@ -26,8 +26,8 @@ export const AdmissionInfo: AdmissionInfo[] = [
 		title: '开学时间 | Start Date',
 		body: (
 			<>
-				<p>2025年9月 (秋季班)</p>
-				<p>September 2025 (Fall term)</p>
+				<p>全年招生，人数到达上限可签waitlist</p>
+				<p>Year-round enrollment, waitlist available upon request</p>
 			</>
 		),
 		icon: Calendar,
@@ -36,8 +36,8 @@ export const AdmissionInfo: AdmissionInfo[] = [
 		title: '上课时间 | Class Time',
 		body: (
 			<>
-				<p>周一至周五 8:30-16:30</p>
-				<p>Monday to Friday, 8:30 am - 4:30 pm</p>
+				<p>周一至周五 8:00-16:00上下</p>
+				<p>Monday to Friday, 8:00 am - 4:00 pm</p>
 			</>
 		),
 		icon: AlarmClock,
@@ -46,8 +46,8 @@ export const AdmissionInfo: AdmissionInfo[] = [
 		title: '班级规模 | Class Size',
 		body: (
 			<>
-				<p>小班制, 每班不超过20人</p>
-				<p>Small classes, no more than 20 children per class</p>
+				<p>小班制, 每班不超过10人</p>
+				<p>Small classes, no more than 10 children per class</p>
 			</>
 		),
 		icon: School,
