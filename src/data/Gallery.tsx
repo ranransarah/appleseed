36bf0206a942e083b214cd6d3e64317c2cdf5136
Steph Xu <stephxu@Stephs-MacBook-Pro.local>
2025-07-18 00:01:41 -0400

    add photos

--- src/data/Gallery.tsx
+++ src/data/Gallery.tsx
@@ -3,7 +3,6 @@ import type { Photo } from 'react-photo-album';
 export const Images: Photo[] = [
 	{ src: '/gallery/1.jpg', width: 400, height: 300 },
 	{ src: '/gallery/8.jpg', width: 600, height: 800 },
-	{ src: '/gallery/2.jpg', width: 400, height: 300 },
 	{ src: '/gallery/3.jpg', width: 800, height: 600 },
 	{ src: '/gallery/5.jpg', width: 400, height: 300 },
 	{ src: '/gallery/6.jpg', width: 400, height: 300 },
@@ -11,4 +10,13 @@ export const Images: Photo[] = [
 	{ src: '/gallery/10.jpg', width: 400, height: 300 },
 	{ src: '/gallery/9.jpg', width: 400, height: 300 },
 	{ src: '/gallery/11.jpg', width: 400, height: 300 },
+	{ src: '/gallery/12.jpg', width: 600, height: 800 },
+	{ src: '/gallery/13.jpg', width: 600, height: 800 },
+	{ src: '/gallery/14.jpg', width: 600, height: 800 },
+	{ src: '/gallery/15.jpg', width: 600, height: 800 },
+	{ src: '/gallery/16.jpg', width: 600, height: 800 },
+	{ src: '/gallery/18.jpg', width: 600, height: 800 },
+	{ src: '/gallery/19.jpg', width: 400, height: 300 },
+	{ src: '/gallery/20.jpg', width: 600, height: 800 },
+	{ src: '/gallery/21.jpg', width: 600, height: 800 },
 ];
